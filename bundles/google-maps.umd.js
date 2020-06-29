26e26a502399f17af8ac5289cd393b3f45d384a8
Jeremy Elbourn <jelbourn@google.com>
2020-06-29 23:02:21 +0000

    10.0.x - 55d50750e chore: bump version to 10.0.1 w/ changelog

--- bundles/google-maps.umd.js
+++ bundles/google-maps.umd.js
@@ -331,7 +331,7 @@
         function GoogleMap(_elementRef, _ngZone, 
         /**
          * @deprecated `platformId` parameter to become required.
-         * @breaking-change 10.0.0-sha-ad231390b
+         * @breaking-change 10.0.0
          */
         platformId) {
             this._elementRef = _elementRef;
@@ -435,7 +435,7 @@
              * https://developers.google.com/maps/documentation/javascript/reference/map#Map.zoom_changed
              */
             this.zoomChanged = this._eventManager.getLazyEmitter('zoom_changed');
-            // @breaking-change 10.0.0-sha-ad231390b Remove null check for `platformId`.
+            // @breaking-change 10.0.0 Remove null check for `platformId`.
             this._isBrowser =
                 platformId ? common.isPlatformBrowser(platformId) : typeof window === 'object' && !!window;
             if (this._isBrowser) {

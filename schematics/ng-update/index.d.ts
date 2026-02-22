cff9533a8d327dcd17be79bb59a7f16501707dc5
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-02-22 07:40:12 +0000

    21.2.x - ef2f1cb build: set up schematics for v22 (#32806)

--- schematics/ng-update/index.d.ts
+++ schematics/ng-update/index.d.ts
@@ -6,5 +6,7 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 import { Rule } from '@angular-devkit/schematics';
-/** Entry point for the migration schematics with target of Angular Material v21 */
+/** Entry point for the migration schematics with target of Google Maps v21 */
 export declare function updateToV21(): Rule;
+/** Entry point for the migration schematics with target of Google Maps v22 */
+export declare function updateToV22(): Rule;
